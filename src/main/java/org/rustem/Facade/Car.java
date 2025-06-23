55f7c74ff65ce5b69fdfe5999d7f0849a5eddbd9
Manga <rustem.ramazanov.66@mail.ru>
2025-06-23 20:03:23 +0300

    Паттерн Facade

---
+++ src/main/java/org/rustem/Facade/Car.java
@@ -0,0 +1,22 @@
+package org.rustem.Facade;
+
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public Car(){
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/org/rustem/Facade/CarRunner.java
@@ -0,0 +1,9 @@
+package org.rustem.Facade;
+
+public class CarRunner {
+    public static void main(String[] args) {
+        CarSale carSale = new CarSale();
+
+        carSale.saleCar("Lexus");
+    }
+}
