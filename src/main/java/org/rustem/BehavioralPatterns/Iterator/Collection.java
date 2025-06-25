9a9e518e4979324644922e32d210b9b7ca83da73
Manga <rustem.ramazanov.66@mail.ru>
2025-06-25 19:37:28 +0300

    Паттерн Iterator

---
+++ src/main/java/org/rustem/BehavioralPatterns/Iterator/Collection.java
@@ -0,0 +1,5 @@
+package org.rustem.BehavioralPatterns.Iterator;
+
+public interface Collection {
+    Iterator getIterator();
+}
---
+++ src/main/java/org/rustem/BehavioralPatterns/Iterator/Iterator.java
@@ -0,0 +1,6 @@
+package org.rustem.BehavioralPatterns.Iterator;
+
+public interface Iterator {
+    boolean hasNext();
+    Object next();
+}
